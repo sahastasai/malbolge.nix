0f26e214760a0345513f6714737f2eb3c259b032
sahastasai <sahanavsairamesh@gmail.com>
2026-08-22 00:23:38 -0700

    implemented some core memory handling logic

--- flake.nix
+++ flake.nix
@@ -11,32 +11,31 @@
     let
       pkgs = import <nixpkgs> {};
       lib = pkgs.lib;
+      malbolgeLength = 59048;
       supportedSystems = [ "x86_64-linux" "aarch64-darwin" "x86_64-darwin" "aarch64-linux" ];
       forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
         pkgs = import nixpkgs { inherit system; };
       });
       stdout = x : builtins.trace "${toString x}" (x);
       filein = x : (builtins.readFile x);
-      # Trits have a and b, because you need 2 bits to hold a trit.
-      # When working with trits, you must input two booleans.
-      # The sequence ab has a as MSB.
-      trit_creator = x : (y : if (builtins.typeOf x == "bool" && builtins.typeOf y == "bool" && !(x && y)) then { a = x; b = y; } else {a = false; b = false;});
-      trit_updator = memory : new_trit : index : (lib.lists.imap0(i : v : if i == index then new_trit else v) memory);
-      trit_access = memory : index : builtins.elemAt memory index;
-      mem = builtins.genList(x : { a = false; b = false; }) 59049;
-      y = f : (x : f (x x)) (x : f (x x));
+      fileinChars = x : builtins.stringToCharacters (filein x);
+      fileinCharsNoSpace = x : builtins.filter (y : y != " ") (fileinChars x);
+      fileinIntsNoSpace = x : map (y : lib.strings.charToInt y) (fileinCharsNoSpace x); 
+      mem = builtins.genList(x : { a = false; b = false; }) malbolgeLength;
+      path = "./me.mb";
+      fileLength = builtins.stringLength (fileinCharsNoSpace path);
+      rawFileLength = builtins.stringLength (fileinChars path);
+      initialMemory = builtins.genList(i : (if i < fileLength then (builtins.elemAt (fileinIntsNoSpace path) i) else 0)) malbolgeLength;
+      transformedMemory = lib.lists.imap0 (i : v : if i < fileLength then v else (op (builtins.elemAt transformedMemory (i - 1)) (builtins.elemAt transformedMemory (i - 2)))) initialMemory;
       xlat1 =
         "+b(29e*j1VMEKLyC})8&m#~W>qxdRp0wkrUo[D7,XTcA\"lI"
         + ".v%{gJh4G\\-=O@5`_3i<?Z';FNQuY]szf$!BS/|t:Pn6^Ha";
-    
       xlat2 =
         "5z]&gqtyfr$(we4{WP)H-Zn,[%\\3dL+Q;>U!pJS72FhOA1C"
         + "B6v^=I_0/8|jsb9m<.TVac`uY*MK'X~xDl}REokN:#?G\"i@";
-    
       # create character arrays
       xlat1List = builtins.stringToCharacters xlat1;
       xlat2List = builtins.stringToCharacters xlat2;
-    
       # determine element at index 1-94 for xlat1
       decode =
         charValue: c:
@@ -44,9 +43,16 @@
           index = builtins.mod (charValue - 33 + c) 94;
         in
         builtins.elemAt xlat1List index;
-    
       # determine element at index 1-94 for xlat2
       mutate = charValue: builtins.elemAt xlat2List (charValue - 33);
+      pvm = index : builtins.elemAt (lib.lists.imap0 (i : v : (decode v i)) (fileinChars path)) index;
+      validMap = lib.lists.imap0 (i : v : ((pvm i) == "j" || (pvm i) == "i" || (pvm i) == "*" || (pvm i) == "p" || (pvm i) == "<" || (pvm i) == "/" || (pvm i) == "v" || (pvm i) == "o")) (builtins.genList (i : i) rawFileLength);
+      # checks if the file is valid
+      validFile = builtins.foldl' (acc : x : acc && x) true validMap;
+      exec = memory : {
+	
+      };
+
     in {	
       schemas = flake-schemas.schemas;
       devShells = forEachSupportedSystem ({ pkgs }: {
