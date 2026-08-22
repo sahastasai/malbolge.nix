60bc48d3dedef518baad25bace76445dc05f3348
sahastasai <sahanavsairamesh@gmail.com>
2026-08-21 20:27:59 -0700

    added memory management

--- flake.nix
+++ flake.nix
@@ -9,7 +9,8 @@
 
   outputs = { self, flake-schemas, nixpkgs }:
     let
-
+      pkgs = import <nixpkgs> {};
+      lib = pkgs.lib;
       supportedSystems = [ "x86_64-linux" "aarch64-darwin" "x86_64-darwin" "aarch64-linux" ];
       forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
         pkgs = import nixpkgs { inherit system; };
@@ -18,9 +19,10 @@
       filein = x : (builtins.readFile x);
       # Trits have a and b, because you need 2 bits to hold a trit.
       # When working with trits, you must input two booleans.
-      # The convention is that the sequence ab is little-endian, with b as LSB.
-      trit_creator = x : (y : if (builtins.typeOf x == "bool" && builtins.typeOf y == "bool" && !(x && y)) then {a = x; b = y; } else {a = false; b = false;});
-
+      # The sequence ab has a as MSB.
+      trit_creator = x : (y : if (builtins.typeOf x == "bool" && builtins.typeOf y == "bool" && !(x && y)) then { a = x; b = y; } else {a = false; b = false;});
+      trit_updator = memory : new_trit : index : (lib.lists.imap0(i : v : if i == index then new_trit else v ) memory);
+      mem = builtins.genList(x : { a = false; b = false; }) 59049;
     in {
 
       schemas = flake-schemas.schemas;
