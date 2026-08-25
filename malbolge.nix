0c6d62b9951c561698891f1a274363e902166125
suyef <yusefray@proton.me>
2026-08-25 15:04:19 -0700

    fix op matrix

--- malbolge.nix
+++ malbolge.nix
@@ -46,15 +46,15 @@
 
   # lookup table determining base9 digit from input, base9 to represent 2 trits
   o = [ 
-  4 3 3 1 0 0 1 0 0 4 
-  3 5 1 0 2 1 0 2 5 5 
-  4 2 2 1 2 2 1 4 3 3 
-  1 0 0 7 6 6 4 3 5 1 
-  0 2 7 6 6 4 3 5 1 0 
-  2 7 6 8 5 5 4 2 2 1 
-  8 8 7 7 6 6 7 6 6 4 
-  3 3 7 6 8 7 6 8 4 3 
-  5 8 8 7 8 8 7 5 5 4 
+  4 3 3 1 0 0 1 0 0 
+  4 3 5 1 0 2 1 0 2 
+  5 5 4 2 2 1 2 2 1
+  4 3 3 1 0 0 7 6 6 
+  4 3 5 1 0 2 7 6 8 
+  5 5 4 2 2 1 8 8 7 
+  7 6 6 7 6 6 4 3 3 
+  7 6 8 7 6 8 4 3 5
+  8 8 7 8 8 7 5 5 4 
   ];
 
   # actual crazy op; convert digit to base9 using lut
