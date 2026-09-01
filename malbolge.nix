d16e83e90c5d60eb65485a51f875f11690a51365
sahastasai <sahanavsairamesh@gmail.com>
2026-09-01 11:55:02 -0700

    wip: begin checks for Int consistency and purge usage of chars

--- malbolge.nix
+++ malbolge.nix
@@ -1,5 +1,5 @@
 # TODO: Migrate all unnecessary char usages to int
-{ pkgs, lib, path ? "./main.mb", ... }: let
+{ pkgs, lib, path ? ./main.mb, ... }: let
   malbolgeLength = 59049;
   stdout = x : builtins.trace "output: ${x}";
   # stdout = x: let
@@ -15,9 +15,7 @@
   fileinCharsNoSpace = x: builtins.filter (y: y != " " && y != "\n" && y != "\t" && y != "\r") (fileinChars x);
   fileinIntsNoSpace = x: map lib.strings.charToInt (fileinCharsNoSpace x); # use EVERYWHERE
   mem = builtins.genList(x : { a = false; b = false; }) malbolgeLength;
-  path = "./main.mb";
-  stdin_path = "./stdin.txt";
-  # main.mbstdin is a stdin replicator
+  stdin_path = "./stdin.txt"; # add to function call eventually
   stdin_parsed = fileinInts stdin_path;
   fileLength = builtins.length (fileinCharsNoSpace path); # use EVERYWHERE
   rawFileLength = builtins.length (fileinChars path);
@@ -34,13 +32,13 @@
   xlat2List = builtins.stringToCharacters xlat2;
   # determine element at index 1-94 for xlat1
   decode =
-    charValue: c:
+    intValue: c:
     let
-      index = lib.trivial.mod (charValue - 33 + c) 94;
+      index = lib.trivial.mod (intValue - 33 + c) 94;
     in
     builtins.elemAt xlat1List index;
   # determine element at index 1-94 for xlat2
-  mutate = charValue: lib.strings.charToInt (builtins.elemAt xlat2List (charValue - 33)); # fixes one character string addition to memory
+  mutate = intValue: (builtins.elemAt xlat2List (intValue - 33)); # fixes one character string addition to memory
   pvm = index : builtins.elemAt (lib.lists.imap0 (i : v : if v > 33 && v < 127 then (decode v i) else 0) (fileinIntsNoSpace path)) index; # potentially problematic line 
     validMap = lib.lists.imap0 (i : v : ((pvm i) == "j" || (pvm i) == "i" || (pvm i) == "*" || (pvm i) == "p" || (pvm i) == "<" || (pvm i) == "/" || (pvm i) == "v" || (pvm i) == "o")) (builtins.genList (i : i) fileLength); # this line could use some cleaning. using `fileLength` everywhere 
   # checks if the file is valid
@@ -87,27 +85,27 @@
       if !isValid then
   	throw "invalid memory value ${toString memc} at C=${toString c}"
       # Integers are required 
-      else if cmd == "v" then 
+      else if cmd == 118 then 
 	builtins.break(stdout out) # breakpoint
       else 
 	let
 	  memd = builtins.break (builtins.elemAt mem d); # breakpoint added for debugging purposes 
 	  
 	  step = 
-	    if cmd == "j" then { d = memd; }
-	    else if cmd == "i" then { c = memd; }
-	    else if cmd == "*" then 
+	    if cmd == 106 then { d = memd; }
+	    else if cmd == 105 then { c = memd; }
+	    else if cmd == 42 then 
 	      let 
 		rot = (memd / 3) + ((lib.trivial.mod memd 3) * 19683);
 	      in
 		{a = rot; mem = lib.lists.replaceElemAt mem d rot;}
-	    else if cmd == "p" then 
+	    else if cmd == 112 then 
 	      let 
 		res = op a memd;
 	      in 
 		{a = res; mem = lib.lists.replaceElemAt mem d res;}
-	    else if cmd == "<" then {out = out + (lib.strings.charFromInt (lib.trivial.mod a 256));}
-	    else if cmd == "/" then 
+	    else if cmd == 60 then {out = out + (lib.strings.charFromInt (lib.trivial.mod a 256));}
+	    else if cmd == 47 then 
 	      if instream < builtins.length stdin_parsed then { 
 		a = builtins.break(builtins.elemAt stdin_parsed instream); # breakpoint
 		instream = instream + 1;
